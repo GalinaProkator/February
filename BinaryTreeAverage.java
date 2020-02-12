6da9e2a521249f1551d3b05031c6b083aed371c3
GalinaProkator <59877586+GalinaProkator@users.noreply.github.com>
2020-02-12 14:04:08 +0200

    Add files via upload

--- BinaryTreeAverage.java
+++ BinaryTreeAverage.java
@@ -9,23 +9,30 @@ public class BinaryTreeAverage {
 		Node node3 = new Node (75, node6, node7);
 		Node node2 = new Node (25, node4, node5);
 		Node node1 = new Node (50,node2, node3);
-		
+
 		System.out.println(binaryTreeAmountOfMembers(node1));
 		System.out.println(binaryTreeSum(node1));
-		
+
 		int binaryTreeAverage = binaryTreeSum(node1)/binaryTreeAmountOfMembers(node1);
 		System.out.println(binaryTreeAverage);
 
 	}
-	
+
 	public static int binaryTreeAmountOfMembers(Node node) {
+		if (node == null) {
+			return 0;
+		}
+
 		if (node.getLeft() == null && node.getRight()==null) {
 			return 1;
 		}
 		return 1+ binaryTreeAmountOfMembers(node.getLeft()) + binaryTreeAmountOfMembers(node.getRight());
 	}
-	
+
 	public static int binaryTreeSum (Node node) {
+		if (node == null) {
+			return 0;
+		}
 		if (node.getLeft() == null && node.getRight()==null) {
 			return node.getValue();
 		}
