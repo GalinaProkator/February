ff0837e3e6295593f64111901f4f4173d2c834fa
GalinaProkator <59877586+GalinaProkator@users.noreply.github.com>
2020-02-21 22:52:28 +0200

    Add files via upload

---
+++ PermutationABC.java
@@ -0,0 +1,49 @@
+import java.util.Random;
+
+public class PermutationABC {
+
+	public static void main(String[] args) {
+		char letters [] = {'a', 'b', 'c'};
+
+		permutation (letters);
+	}
+
+	public static void permutation (char [] letters) {
+		char newArray [] = new char [letters.length];
+		int currentCell = 0;
+
+		//		get a random number within a range 0-2
+		while (currentCell < 3) {
+			int random = getRandom();
+			while (isPresentInTheArray(letters[random], newArray)) {
+				random = getRandom();
+			}
+			newArray [currentCell] = letters[random];
+			currentCell++;
+		}
+		printArray(newArray);
+	}
+
+	private static boolean isPresentInTheArray (char random, char [] newArray) {
+		for (int i=0; i<newArray.length; i++) {
+			if (newArray[i] == random) {
+				return true;
+			}
+		}
+		return false;
+	}
+
+	private static int getRandom() {
+		Random rnd = new Random();
+		int random = rnd.nextInt(3);
+		return random;
+	}
+
+	private static void printArray (char [] newArray) {
+		for (int i=0; i<newArray.length; i++) {
+			System.out.print(newArray[i] + " ");
+		}
+	}
+
+
+}
